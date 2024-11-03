1173a2f82e658ede1643505177ca0c353748e123
Jae-jun Kang <jbluefire@gmail.com>
2024-11-03 19:14:40 +0900

    Update Breadboard.Compiler.Program class

---
+++ compiler/Compiler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.CommandLine;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Breadboard.Compiler
+{
+    class Compiler
+    {
+        public bool Recursive { get; set; }
+
+        public void Process(List<string> arguments)
+        {
+            Console.WriteLine(Recursive);
+            foreach (var argument in arguments)
+            {
+                Console.WriteLine(argument);
+            }
+        }
+    }
+}
--- compiler/Program.cs
+++ compiler/Program.cs
@@ -1,14 +1,57 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
 
 namespace Breadboard.Compiler
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int errorCount;
+
+        public static void IncrementErrorCount()
         {
-            Console.WriteLine("Hello, World!");
+            Interlocked.Increment(ref errorCount);
+        }
+
+        static int Main(string[] args)
+        {
+            var arguments = new Argument<List<string>>(
+                name: "argument",
+                description: "File or directory",
+                getDefaultValue: () => { return [ "." ]; });
+
+            var recursiveOption = new Option<bool>(
+                aliases: [ "-r", "--recursive" ],
+                description: "Process subdirectories recursively");
+
+            var rootCommand = new RootCommand("Breadboard definition compiler");
+            rootCommand.AddArgument(arguments);
+            rootCommand.AddOption(recursiveOption);
+
+            rootCommand.SetHandler((arguments, recursive) =>
+            {
+                var compiler = new Compiler {
+                    Recursive = recursive
+                };
+
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                compiler.Process(arguments);
+
+                stopWatch.Stop();
+                Console.WriteLine(errorCount == 0 ? "Ok" : $"{errorCount} error(s)");
+                Console.WriteLine($"Elapsed {stopWatch.ElapsedMilliseconds} ms");
+            }
+            , arguments, recursiveOption);
+
+            int exitCode = rootCommand.Invoke(args);
+            return exitCode != 0 ? exitCode : errorCount;
         }
     }
 }
